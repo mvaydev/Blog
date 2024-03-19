25e3ed8ab8f7ba15a9060df1fa3e01eaeadd70b3
Maksim Vaysbekker <mvaydev@gmail.com>
2024-03-19 18:22:24 +0800

    Configure frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+	content: [
+		"./index.html",
+		"./src/**/*.{js,jsx,html}",
+	],
+	theme: {
+		colors: {
+			'red': '#F15757',
+		},
+		fontFamily: {
+			sans: ['Inter', 'sans-serif'],
+		},
+	},
+	plugins: [],
+}
+
