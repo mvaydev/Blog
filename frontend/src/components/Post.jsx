ec125c19b527c385f4b29720905eef04797a4b06
Maksim Vaysbekker <mvaydev@gmail.com>
2024-03-25 21:00:35 +0800

    Create main page layout

---
+++ frontend/src/components/Post.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import Block from '../components/Block'
+import { Link } from 'react-router-dom'
+
+import Like from '../assets/img/heart.svg'
+import Liked from '../assets/img/heart_filled.svg'
+import Comment from '../assets/img/comment.svg'
+
+function LikeButton(props) {
+    const [likes, setLikes] = useState(props.likes)
+    const [isLikesChanged, setIsLikesChanged] = useState(false)
+
+    return (
+        <button 
+            className='rounded-full bg-stone-200 py-1.5 px-3 w-fit flex gap-1.5 hover:bg-stone-300'
+            onClick={() => {
+                setIsLikesChanged(!isLikesChanged) 
+                !isLikesChanged ? setLikes(likes + 1) : setLikes(likes - 1)
+            }}
+        >
+            <span className='text-stone-500'>{likes}</span>
+            {
+                !isLikesChanged ?
+                <img src={Like} /> :
+                <img src={Liked} />
+            }
+        </button>
+    )
+}
+
+export default (props) => {
+    return (
+        <>
+            <Block>
+                <div className='flex gap-1.5 items-center w-full'>
+                    <p className='font-medium'>{props.author}</p>
+                    <p className='text-sm text-stone-500 '>{props.createdAt}</p>
+                </div>
+                <h1 className='text-xl font-bold'>{props.title}</h1>
+                <p>{props.content}</p>
+                <div className='flex gap-1.5'>
+                    <LikeButton likes={props.likes} />
+                    <Link 
+                        to={'/post/' + props.id + '/#comments'} 
+                        replace
+                        className='rounded-full bg-stone-200 py-1.5 px-3 w-fit flex gap-1.5 hover:bg-stone-300'
+                    >
+                        <span className='text-stone-500'>{props.comments}</span>
+                        <img src={Comment} />
+                    </Link>
+                </div>
+            </Block>
+        </>
+    )
+}
--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,7 +1,79 @@
-import Navbar from '../components/Navbar';
+import Navbar from '../components/Navbar'
+import Post from '../components/Post'
+
+const POSTS = [
+    {
+        author: 'Maksim Vaysbekker',
+        createdAt: '12.09.2024 в 12:07',
+        title: 'Пост №1',
+        content: 'Lorem ipsum dolor sit amet',
+        likes: 12,
+        comments: 3
+    },
+    {
+        author: 'Maksim Vaysbekker',
+        createdAt: '12.09.2024 в 12:07',
+        title: 'Пост №2',
+        content: 'Lorem ipsum dolor sit amet',
+        likes: 12,
+        comments: 3
+    },
+    {
+        author: 'Maksim Vaysbekker',
+        createdAt: '12.09.2024 в 12:07',
+        title: 'Пост №3',
+        content: 'Lorem ipsum dolor sit amet',
+        likes: 12,
+        comments: 3
+    },
+    {
+        author: 'Maksim Vaysbekker',
+        createdAt: '12.09.2024 в 12:07',
+        title: 'Пост №4',
+        content: 'Lorem ipsum dolor sit amet',
+        likes: 12,
+        comments: 3
+    },
+    {
+        author: 'Maksim Vaysbekker',
+        createdAt: '12.09.2024 в 12:07',
+        title: 'Пост №5',
+        content: 'Lorem ipsum dolor sit amet',
+        likes: 12,
+        comments: 3
+    },
+    {
+        author: 'Maksim Vaysbekker',
+        createdAt: '12.09.2024 в 12:07',
+        title: 'Пост №6',
+        content: 'Lorem ipsum dolor sit amet',
+        likes: 12,
+        comments: 3
+    },
+]
 
 export default () => {
     return (
-        <Navbar />
+        <>
+            <Navbar />
+            <div className='flex flex-col gap-8 w-full items-center mt-8'>
+                
+
+                {
+                    POSTS.map((post, index) => 
+                        <Post 
+                            key={index} 
+                            author={post.author} 
+                            createdAt={post.createdAt} 
+                            title={post.title} 
+                            content={post.content} 
+                            likes={post.likes} 
+                            comments={post.comments} 
+                            id={index}
+                        />
+                    )
+                }
+            </div>
+        </>
     )
 }
